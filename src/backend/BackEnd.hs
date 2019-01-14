4eb9781eb8cdf1df6f04062647d236166e701be7
m-naruniec <micnar12@gmail.com>
2019-01-14 12:51:29 +0100

    Remove debug prints

--- src/backend/BackEnd.hs
+++ src/backend/BackEnd.hs
@@ -23,44 +23,15 @@ import LLVM
 backEnd :: Program () -> IO String
 backEnd p = do
       let (ctx, quadCode) = genQuad p
-      sequence_ $ printList . snd <$> quadCode
       let blockFuns = quadToBlocks ctx quadCode
-      putStrLn "\n\n"
-      printQCode blockFuns
       let blockFuns' = removeNops blockFuns
-      putStrLn "\n\nAfter nop elimination:\n"
-      printQCode blockFuns'
       let connections = findConns blockFuns'
-      putStrLn $ show connections
-      putStrLn "\n\n"
---      let reachable = reachableLabels blockFuns' connections
---      putStrLn $ show reachable
-
       let (blockFuns'', connections') = removeDeadNodes blockFuns' connections
-      putStrLn "\n\n"
-      printQCode blockFuns''
-      putStrLn $ show connections'
-
       let graph = toGraph blockFuns'' connections'
-      putStrLn "\n\n"
-      putStrLn $ show graph
-
       let liveMap = getLiveVars graph
-      putStrLn "\n\n"
-      putStrLn $ show liveMap
-
       let ssaGraph = makeSSA graph liveMap
-      putStrLn "\n\n"
-      printQCode $ fst $ fromGraph ssaGraph
-
       optGraph <- optimize ssaGraph
-      putStrLn "\n\n"
-      printQCode $ fst $ fromGraph optGraph
-
       let linear = fst $ fromGraph optGraph
       let llvmCode = getLLVM linear
-
-      printStringList llvmCode
-
       return $ unlines llvmCode
 
--- src/backend/Optimize.hs
+++ src/backend/Optimize.hs
@@ -19,26 +19,13 @@ import DeadCode
 optimize :: Graph -> IO Graph
 optimize graph = do
       let (graph1, changed1) = foldAtoms graph
---      putStrLn $ "\n\nafter fold: \n"
---      printQCode $ fst $ fromGraph graph1
       let (linear, conns) = fromGraph graph1
       let (linear', conns') = removeDeadNodes linear conns
---      putStrLn $ "\n\nafter dead nodes: \n"
---      printQCode linear'
       let graph2 = toGraph linear' conns'
       let graph3 = updatePhi graph2
---      putStrLn $ "\n\nafter phi update: \n"
---      printQCode $ fst $ fromGraph graph3
       let liveMap = getLiveVars graph3
---      putStrLn $ "\n\nlive vars: \n" ++ show liveMap
       let (graph4, changed2) = removeDeadCode graph3 liveMap
---      putStrLn $ "\n\nafter dead code: \n"
---      printQCode $ fst $ fromGraph graph4
-
       if changed1 || changed2
         then optimize graph4
         else return graph4
-
-
-
 
